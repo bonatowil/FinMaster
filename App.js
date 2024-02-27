9769307f64957d4b51ba790aa705bfe88fa90c8b
Wilson Bonato <wilsonbonatojr@gmail.com>
2024-02-27 14:21:30 -0300

    Add suport to Google Fonts - Inter font

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';  
+import { useFonts, Inter_300Light } from '@expo-google-fonts/inter';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_300Light,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Inter_300Light' }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
