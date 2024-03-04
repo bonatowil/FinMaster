eda4a455bcfd32711849543268c4e9a11edd04cb
Wilson Bonato <wilsonbonatojr@gmail.com>
2024-03-04 00:18:27 -0300

    Start to implement design to app

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,25 @@
-import { View, Text, ScrollView} from "react-native";
+import { View, Text, ScrollView, TouchableOpacity} from "react-native";
 import style from "./style";
 
 export default function Main() {
     return (
         <View style={style.mainContainer}>
+        <View style={style.saldo}>
+            <Text style={style.saldoLabel}>Saldo total</Text>
+            <Text style={style.saldoText}>R$ 2.530,37</Text>
+        </View>
             <View style={style.scrollerWrap}>
                 <ScrollView style={style.scroller}>
+                    <View style={style.quickAccess}>
+                        <Text style={style.quickAccessLabel}>Acesso Rápido</Text>
+                        <ScrollView style={style.quickAccessButtons} horizontal={true}>
+                            <TouchableOpacity style={style.quickAccessButton}></TouchableOpacity>
+                            <TouchableOpacity style={style.quickAccessButton}></TouchableOpacity>
+                            <TouchableOpacity style={style.quickAccessButton}></TouchableOpacity>
+                            <TouchableOpacity style={style.quickAccessButton}></TouchableOpacity>
+                            <TouchableOpacity style={style.quickAccessButton}></TouchableOpacity>
+                        </ScrollView>
+                    </View>
                 </ScrollView>    
             </View>
         </View>
--- src/components/Main/style.js
+++ src/components/Main/style.js
@@ -2,19 +2,55 @@ import { StyleSheet } from "react-native";
 
 const style = StyleSheet.create({
     mainContainer: {
-        backgroundColor: '#003049',
+        backgroundColor: '#00B9AE',
         height: '100%',
         width: '100%',
-        justifyContent: 'flex-end',
+        justifyContent: 'space-between',
     },
     scrollerWrap: {
-        backgroundColor: '#E7E7E7',
-        height: '80%',
-        borderTopStartRadius: 30,
-        borderTopEndRadius: 30,
+        backgroundColor: '#FFFFF3',
+        height: '75%',
+        borderTopStartRadius: 50,
+        borderTopEndRadius: 50,
     },
-    scoller: {
-        backgroundColor: 'black',
+    saldo: {
+        width: '100%',
+        height: '25%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    saldoText: {
+        color: '#FFFFF3',
+        fontFamily: 'Inter_700Bold',
+        fontSize: 40,
+    },
+    saldoLabel: {
+        color: '#FFFFF3',
+        fontFamily: 'Inter_300Light',
+        fontSize: 15,
+    },
+    quickAccess: {
+    },
+    quickAccessLabel: {
+        paddingTop: 20,
+        paddingLeft: 20,
+        color: '#171313',
+        fontFamily: 'Inter_700Bold',
+        fontSize: 17,
+        opacity: .75,
+    },
+    quickAccessButtons: {
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        width: '100%',
+    },
+    quickAccessButton: {
+        height: 80,
+        width: 80,
+        marginRight: 15,
+        borderRadius: 20,
+        borderColor: '#171313', 
+        borderWidth: 2,
     }
 })
 
